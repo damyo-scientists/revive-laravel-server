653b50829c2452ef5c0901f77399acce60dd80ea
Horace Velmont <dragond7@naver.com>
2019-05-24 14:44:51 +0900

    initial setting

---
+++ app/Models/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Velmont
+ * Date: 2019-05-17
+ * Time: 오후 5:14
+ */
+
+namespace App\Models;
+
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
+
+
+class User extends Eloquent
+{
+
+}
